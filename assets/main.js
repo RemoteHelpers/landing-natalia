889a6fa65a4e8c889fda5c81ce6a5a8a64bc701d
NataliaTronza <melezh.nataliya@gmail.com>
2022-07-28 19:15:50 +0300

    block reasons

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,8 @@
-const root = document.documentElement;
-const marqueeContent = document.querySelector('ul.running-string__list'); 
-root.style.setProperty('--marquee-elements', marqueeContent.children.length);
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue('--marquee-elements-displayed');
+// const root = document.documentElement;
+// const marqueeContent = document.querySelector('ul.running-string__list'); 
+// root.style.setProperty('--marquee-elements', marqueeContent.children.length);
+// const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue('--marquee-elements-displayed');
 
-for(let index = 0; index < marqueeElementsDisplayed.length; index++) {
-    marqueeContent.appendChild(marqueeContent.children[index].cloneNode(true))
-}  
+// for(let index = 0; index < 4; index++) {
+//     marqueeContent.appendChild(marqueeContent.children[index].cloneNode(true))
+// }  
