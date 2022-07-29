749743d9237ed08b510a468843f8facdbd82a776
NataliaTronza <melezh.nataliya@gmail.com>
2022-07-29 12:22:07 +0300

    бегущая строка

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,3 @@
-// const root = document.documentElement;
-// const marqueeContent = document.querySelector('ul.running-string__list'); 
-// root.style.setProperty('--marquee-elements', marqueeContent.children.length);
-// const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue('--marquee-elements-displayed');
-
-// for(let index = 0; index < 4; index++) {
-//     marqueeContent.appendChild(marqueeContent.children[index].cloneNode(true))
-// }  
+$('#webTicker').webTicker({
+    height:'130px'
+});
